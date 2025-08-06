9dddf65535f0f903782375061da73f2b788ebce5
Seepo <seep0@pm.me>
2025-08-06 21:14:46 +0300

    Update 50-branding.sh

--- files/scripts/50-branding.sh
+++ files/scripts/50-branding.sh
@@ -2,9 +2,9 @@
 
 set -xeuo pipefail
 
-rm -rf /usr/share/wallpapers/Fedora
-rm -rf /usr/share/wallpapers/F4*
-rm -rf /usr/share/backgrounds/f4*
+#rm -rf /usr/share/wallpapers/Fedora
+#rm -rf /usr/share/wallpapers/F4*
+#rm -rf /usr/share/backgrounds/f4*
 
 dnf remove -y \
     console-login-helper-messages
