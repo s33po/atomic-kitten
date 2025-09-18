683edda0b10ade50cf3a3070bd4470bdcd90139d
Seepo <seep0@pm.me>
2025-09-18 19:13:32 +0300

    feat: add backported gnome 48

--- files/scripts/20-desktop.sh
+++ files/scripts/20-desktop.sh
@@ -2,6 +2,10 @@
 
 set -xeuo pipefail
 
+### TESTING: GNOME 48 backport from HyperScale SIG ###
+dnf -y copr enable @centoshyperscale/c10s-gnome-48
+dnf -y install glib2
+
 dnf -y install \
     @"Workstation" \
     @"Virtualization Host" 
