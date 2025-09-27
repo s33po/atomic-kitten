20422aa5805cfb85b43bd254c4f14a9243111e76
Seepo <seep0@pm.me>
2025-09-27 12:16:53 +0300

    feat: add more codecs for thumbnails

--- files/scripts/21-multimedia.sh
+++ files/scripts/21-multimedia.sh
@@ -5,13 +5,13 @@ set -xeuo pipefail
 # Multimedia codecs
 dnf -y install \
     @multimedia \
+    ffmpeg \
+    libavcodec \
     ffmpegthumbnailer \
     gstreamer1-plugins-{base,bad-free-libs} \
     lame{,-libs} \
-    libjxl
-
-# Multimedia (from Bluefin LTS)
-#dnf config-manager --add-repo=https://negativo17.org/repos/epel-multimedia.repo
-#dnf config-manager --set-disabled epel-multimedia
-#dnf -y install --enablerepo=epel-multimedia \
-#	ffmpeg libavcodec @multimedia gstreamer1-plugins-{bad-free,bad-free-libs,good,base} lame{,-libs} libjxl ffmpegthumbnailer
+    libjxl \
+    libheif \
+    libwebp \
+    webp-pixbuf-loader \
+    avif-pixbuf-loader
